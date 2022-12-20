4e113dd5686faf8c36131a74a5bc0a76bbd0798e
Om Suresh Javir <javirom07042003@gmail.com>
2022-12-20 21:03:20 +0530

    Added new image

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -10,42 +10,42 @@ const Projects = () => {
         <div className='w-[90vw] items-center m-auto flex flex-wrap justify-center'>
           <div className='border-gray-300 border-2 rounded-sm w-[85vw] sm:w-[25vw] flex flex-col m-2'>
             <img className='w-full rounded-sm border-b-gray-200 border-2' src="https://github.com/Omjavir/portfolio/blob/master/public/mp1.png?raw=true" alt="Image1" />
-            <h3 className='sm:text-2xl text-xl font-semibold m-2'>To-Do List</h3>
+            <h3 className='sm:text-2xl text-xl font-semibold font-mono m-2'>To-Do List</h3>
             <div className='flex m-auto'>
-              <a target={'_blank'} href="https://omjavir.github.io/To-Do-s-List/"><button className='m-2 bg-transparent border-gray-300 border-2 px-3 sm:text-2xl rounded-md underline'>Live</button></a>
-              <a target={'_blank'} href="https://github.com/Omjavir/To-Do-s-List"><button className='m-2 bg-transparent border-gray-300 border-2 px-3 sm:text-2xl rounded-md underline'>Code</button></a>
+              <a target={'_blank'} href="https://omjavir.github.io/To-Do-s-List/"><button className='m-2 bg-transparent border-gray-300 border-2 px-5 font-mono sm:text-2xl rounded-md underline'>Live</button></a>
+              <a target={'_blank'} href="https://github.com/Omjavir/To-Do-s-List"><button className='m-2 bg-transparent border-gray-300 border-2 px-5 font-mono sm:text-2xl rounded-md underline'>Code</button></a>
             </div>
           </div>
           <div className='border-gray-300 border-2 rounded-sm w-[85vw] sm:w-[25vw] flex flex-col m-2'>
             <img className='w-full rounded-sm border-b-gray-200 border-2' src="https://github.com/Omjavir/portfolio/blob/master/public/mp2.png?raw=true" alt="Image1" />
-            <h3 className='sm:text-2xl text-xl font-semibold m-2'>Responsive Navbar</h3>
+            <h3 className='sm:text-2xl text-xl font-semibold font-mono m-2'>Responsive Navbar</h3>
             <div className='flex m-auto'>
-              <a target={'_blank'} href="https://omjavir.github.io/Responsive-Navbar/"><button className='m-2 bg-transparent border-gray-300 border-2 px-3 sm:text-2xl rounded-md underline'>Live</button></a>
-              <a target={'_blank'} href="https://github.com/Omjavir/Responsive-Navbar"><button className='m-2 bg-transparent border-gray-300 border-2 px-3 sm:text-2xl rounded-md underline'>Code</button></a>
+              <a target={'_blank'} href="https://omjavir.github.io/Responsive-Navbar/"><button className='m-2 bg-transparent border-gray-300 border-2 px-5 font-mono sm:text-2xl rounded-md underline'>Live</button></a>
+              <a target={'_blank'} href="https://github.com/Omjavir/Responsive-Navbar"><button className='m-2 bg-transparent border-gray-300 border-2 px-5 font-mono sm:text-2xl rounded-md underline'>Code</button></a>
             </div>
           </div>
           <div className='border-gray-300 border-2 rounded-sm w-[85vw] sm:w-[25vw] flex flex-col m-2'>
             <img className='w-full rounded-sm border-b-gray-200 border-2' src="https://github.com/Omjavir/portfolio/blob/master/public/mp3.png?raw=true" alt="Image1" />
-            <h3 className='sm:text-2xl text-xl font-semibold m-2'>Image Slider</h3>
+            <h3 className='sm:text-2xl text-xl font-semibold font-mono m-2'>Image Slider</h3>
             <div className='flex m-auto'>
-              <a target={'_blank'} href="https://omjavir.github.io/Image-Slider/"><button className='m-2 bg-transparent border-gray-300 border-2 px-3 sm:text-2xl rounded-md underline'>Live</button></a>
-              <a target={'_blank'} href="https://github.com/Omjavir/Image-Slider"><button className='m-2 bg-transparent border-gray-300 border-2 px-3 sm:text-2xl rounded-md underline'>Code</button></a>
+              <a target={'_blank'} href="https://omjavir.github.io/Image-Slider/"><button className='m-2 bg-transparent border-gray-300 border-2 px-5 font-mono sm:text-2xl rounded-md underline'>Live</button></a>
+              <a target={'_blank'} href="https://github.com/Omjavir/Image-Slider"><button className='m-2 bg-transparent border-gray-300 border-2 px-5 font-mono sm:text-2xl rounded-md underline'>Code</button></a>
             </div>
           </div>
           <div className='border-gray-300 border-2 rounded-sm w-[85vw] sm:w-[25vw] flex flex-col m-2'>
             <img className='w-full rounded-sm border-b-gray-200 border-2' src="https://github.com/Omjavir/portfolio/blob/master/public/mp4.png?raw=true" alt="Image1" />
-            <h3 className='sm:text-2xl text-xl font-semibold m-2'>Real-Time Clock</h3>
+            <h3 className='sm:text-2xl text-xl font-semibold font-mono m-2'>Real-Time Clock</h3>
             <div className='flex m-auto'>
-              <a target={'_blank'} href="http://clock-omjavir.vercel.app/"><button className='m-2 bg-transparent border-gray-300 border-2 px-3 sm:text-2xl rounded-md underline'>Live</button></a>
-              <a target={'_blank'} href="https://github.com/Omjavir/clock"><button className='m-2 bg-transparent border-gray-300 border-2 px-3 sm:text-2xl rounded-md underline'>Code</button></a>
+              <a target={'_blank'} href="http://clock-omjavir.vercel.app/"><button className='m-2 bg-transparent border-gray-300 border-2 px-5 font-mono sm:text-2xl rounded-md underline'>Live</button></a>
+              <a target={'_blank'} href="https://github.com/Omjavir/clock"><button className='m-2 bg-transparent border-gray-300 border-2 px-5 font-mono sm:text-2xl rounded-md underline'>Code</button></a>
             </div>
           </div>
           <div className='border-gray-300 border-2 rounded-sm w-[85vw] sm:w-[25vw] flex flex-col m-2'>
             <img className='w-full rounded-sm border-b-gray-200 border-2' src="https://github.com/Omjavir/portfolio/blob/master/public/mp5.png?raw=true" alt="Image1" />
-            <h3 className='sm:text-2xl text-xl font-semibold m-2'>Toggle Dark Mode</h3>
+            <h3 className='sm:text-2xl text-xl font-semibold font-mono m-2'>Toggle Dark Mode</h3>
             <div className='flex m-auto'>
-              <a target={'_blank'} href="http://dark-mode-toggle-swart.vercel.app/"><button className='m-2 bg-transparent border-gray-300 border-2 px-3 sm:text-2xl rounded-md underline'>Live</button></a>
-              <a target={'_blank'} href="https://github.com/Omjavir/dark-mode-toggle"><button className='m-2 bg-transparent border-gray-300 border-2 px-3 sm:text-2xl rounded-md underline'>Code</button></a>
+              <a target={'_blank'} href="http://dark-mode-toggle-swart.vercel.app/"><button className='m-2 bg-transparent border-gray-300 border-2 px-5 font-mono sm:text-2xl rounded-md underline'>Live</button></a>
+              <a target={'_blank'} href="https://github.com/Omjavir/dark-mode-toggle"><button className='m-2 bg-transparent border-gray-300 border-2 px-5 font-mono sm:text-2xl rounded-md underline'>Code</button></a>
             </div>
           </div>
         </div>
@@ -57,49 +57,49 @@ const Projects = () => {
         <div className='w-[90vw] items-center m-auto flex flex-wrap justify-center'>
           <div className='border-gray-300 border-2 rounded-sm w-[85vw] sm:w-[25vw] flex flex-col m-2'>
             <img className='w-full rounded-sm border-b-gray-200 border-2' src="https://github.com/Omjavir/portfolio/blob/master/public/ls1.png?raw=true" alt="Image1" />
-            <h3 className='sm:text-2xl text-xl font-semibold m-2'>Security Website</h3>
+            <h3 className='sm:text-2xl text-xl font-semibold font-mono m-2'>Security Website</h3>
             <div className='flex m-auto'>
-              <a target={'_blank'} href="https://security-website.vercel.app/"><button className='m-2 bg-transparent border-gray-300 border-2 px-3 sm:text-2xl rounded-md underline'>Live</button></a>
-              <a target={'_blank'} href="https://github.com/Omjavir/security_website"><button className='m-2 bg-transparent border-gray-300 border-2 px-3 sm:text-2xl rounded-md underline'>Code</button></a>
+              <a target={'_blank'} href="https://security-website.vercel.app/"><button className='m-2 bg-transparent border-gray-300 border-2 px-5 font-mono sm:text-2xl rounded-md underline'>Live</button></a>
+              <a target={'_blank'} href="https://github.com/Omjavir/security_website"><button className='m-2 bg-transparent border-gray-300 border-2 px-5 font-mono sm:text-2xl rounded-md underline'>Code</button></a>
             </div>
           </div>
           <div className='border-gray-300 border-2 rounded-sm w-[85vw] sm:w-[25vw] flex flex-col m-2'>
             <img className='w-full rounded-sm border-b-gray-200 border-2' src="https://github.com/Omjavir/portfolio/blob/master/public/ls2.png?raw=true" alt="Image1" />
-            <h3 className='sm:text-2xl text-xl font-semibold m-2'>Netflix UI CLone</h3>
+            <h3 className='sm:text-2xl text-xl font-semibold font-mono m-2'>Netflix UI CLone</h3>
             <div className='flex m-auto'>
-              <a target={'_blank'} href="https://netflix-clone-delta-eight-87.vercel.app/"><button className='m-2 bg-transparent border-gray-300 border-2 px-3 sm:text-2xl rounded-md underline'>Live</button></a>
-              <a target={'_blank'} href="https://github.com/Omjavir/Netflix-clone"><button className='m-2 bg-transparent border-gray-300 border-2 px-3 sm:text-2xl rounded-md underline'>Code</button></a>
+              <a target={'_blank'} href="https://netflix-clone-delta-eight-87.vercel.app/"><button className='m-2 bg-transparent border-gray-300 border-2 px-5 font-mono sm:text-2xl rounded-md underline'>Live</button></a>
+              <a target={'_blank'} href="https://github.com/Omjavir/Netflix-clone"><button className='m-2 bg-transparent border-gray-300 border-2 px-5 font-mono sm:text-2xl rounded-md underline'>Code</button></a>
             </div>
           </div>
           <div className='border-gray-300 border-2 rounded-sm w-[85vw] sm:w-[25vw] flex flex-col m-2'>
             <img className='w-full rounded-sm border-b-gray-200 border-2' src="https://github.com/Omjavir/portfolio/blob/master/public/ls3.png?raw=true" alt="Image1" />
-            <h3 className='sm:text-2xl text-xl font-semibold m-2'>Frest</h3>
+            <h3 className='sm:text-2xl text-xl font-semibold font-mono m-2'>Frest</h3>
             <div className='flex m-auto'>
-              <a target={'_blank'} href="http://frest-fronend.vercel.app/"><button className='m-2 bg-transparent border-gray-300 border-2 px-3 sm:text-2xl rounded-md underline'>Live</button></a>
-              <a target={'_blank'} href="https://github.com/Omjavir/Frest_frontend"><button className='m-2 bg-transparent border-gray-300 border-2 px-3 sm:text-2xl rounded-md underline'>Code</button></a>
+              <a target={'_blank'} href="http://frest-fronend.vercel.app/"><button className='m-2 bg-transparent border-gray-300 border-2 px-5 font-mono sm:text-2xl rounded-md underline'>Live</button></a>
+              <a target={'_blank'} href="https://github.com/Omjavir/Frest_frontend"><button className='m-2 bg-transparent border-gray-300 border-2 px-5 font-mono sm:text-2xl rounded-md underline'>Code</button></a>
             </div>
           </div>
           <div className='border-gray-300 border-2 rounded-sm w-[85vw] sm:w-[25vw] flex flex-col m-2'>
             <img className='w-full rounded-sm border-b-gray-200 border-2' src="https://github.com/Omjavir/portfolio/blob/master/public/ls4.png?raw=true" alt="Image1" />
-            <h3 className='sm:text-2xl text-xl font-semibold m-2'>Finance Website</h3>
+            <h3 className='sm:text-2xl text-xl font-semibold font-mono m-2'>Finance Website</h3>
             <div className='flex m-auto'>
-              <a target={'_blank'} href="http://finance-website-lemon.vercel.app/"><button className='m-2 bg-transparent border-gray-300 border-2 px-3 sm:text-2xl rounded-md underline'>Live</button></a>
-              <a target={'_blank'} href="https://github.com/Omjavir/finance_website"><button className='m-2 bg-transparent border-gray-300 border-2 px-3 sm:text-2xl rounded-md underline'>Code</button></a>
+              <a target={'_blank'} href="http://finance-website-lemon.vercel.app/"><button className='m-2 bg-transparent border-gray-300 border-2 px-5 font-mono sm:text-2xl rounded-md underline'>Live</button></a>
+              <a target={'_blank'} href="https://github.com/Omjavir/finance_website"><button className='m-2 bg-transparent border-gray-300 border-2 px-5 font-mono sm:text-2xl rounded-md underline'>Code</button></a>
             </div>
           </div>
           <div className='border-gray-300 border-2 rounded-sm w-[85vw] sm:w-[25vw] flex flex-col m-2'>
             <img className='w-full rounded-sm border-b-gray-200 border-2' src="https://github.com/Omjavir/portfolio/blob/master/public/ls5.png?raw=true" alt="Image1" />
-            <h3 className='sm:text-2xl text-xl font-semibold m-2'>Ai Image Generator</h3>
+            <h3 className='sm:text-2xl text-xl font-semibold font-mono m-2'>Ai Image Generator</h3>
             <div className='flex m-auto'>
-              <a target={'_blank'} href="http://a-i-image-generator.vercel.app/"><button className='m-2 bg-transparent border-gray-300 border-2 px-3 sm:text-2xl rounded-md underline'>Live</button></a>
-              <a target={'_blank'} href="https://github.com/Omjavir/A.I-Image-Generator"><button className='m-2 bg-transparent border-gray-300 border-2 px-3 sm:text-2xl rounded-md underline'>Code</button></a>
+              <a target={'_blank'} href="http://a-i-image-generator.vercel.app/"><button className='m-2 bg-transparent border-gray-300 border-2 px-5 font-mono sm:text-2xl rounded-md underline'>Live</button></a>
+              <a target={'_blank'} href="https://github.com/Omjavir/A.I-Image-Generator"><button className='m-2 bg-transparent border-gray-300 border-2 px-5 font-mono sm:text-2xl rounded-md underline'>Code</button></a>
             </div>
           </div>
           <div className='border-gray-300 border-2 rounded-sm w-[85vw] sm:w-[25vw] flex flex-col m-2'>
             <img className='w-full rounded-sm border-b-gray-200 border-2' src="https://github.com/Omjavir/portfolio/blob/master/public/ls6.png?raw=true" alt="Image1" />
-            <h3 className='sm:text-2xl text-xl font-semibold m-2'>Messenger</h3>
+            <h3 className='sm:text-2xl text-xl font-semibold font-mono m-2'>Messenger</h3>
             <div className='flex m-auto'>
-              <a target={'_blank'} href="https://github.com/Omjavir/chatapp-frontend"><button className='m-2 bg-transparent border-gray-300 border-2 px-3 sm:text-2xl rounded-md underline'>Code</button></a>
+              <a target={'_blank'} href="https://github.com/Omjavir/chatapp-frontend"><button className='m-2 bg-transparent border-gray-300 border-2 px-5 font-mono sm:text-2xl rounded-md underline'>Code</button></a>
             </div>
           </div>
         </div>
